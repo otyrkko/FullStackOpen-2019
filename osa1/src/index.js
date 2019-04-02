24e671bd1a890fbf4e21d2e0e95dbbe6761b015c
Olli Tyrkkö <olli.tyrkko@gmail.com>
2019-04-02 22:52:17 +0300

    Osa1: upto 'Monta komponenttia'

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+    const now = new Date()
+    const a = 10
+    const b = 20
+    return (    
+        <div>
+            <p>Hello world, it is {now.toString()}</p>
+            <p>{a} plus {b} is {a + b}</p>
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
